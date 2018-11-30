a7fc4adca9f7bbf0de9ee51390265ed3d9b5f19b
Brian Roepke <broepke@gmail.com>
2018-11-30 14:12:42 -0800

    Initial Forge Call - this Authenticates

---
+++ other/forge.py
@@ -0,0 +1,12 @@
+# this one authenticates with my test account
+
+curl - v
+'https://developer.api.autodesk.com/authentication/v1/authenticate' - X
+'POST' - H
+'Content-Type: application/x-www-form-urlencoded' - d
+'client_id=0njO39Ges7ueLAbOB3eVYNsW8venF2mJ' - d
+'client_secret=LPrOXsCgEgUzVHwv' - d
+'grant_type=client_credentials' - d
+'scope=data:read' - d
+'scope=data:write'
+

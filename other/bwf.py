594304b510358eb09046e918a775df89ef31f5a6
Brian Roepke <broepke@gmail.com>
2018-12-14 10:05:58 -0800

    Fixed the BWF dictionary to a single object

--- other/bwf.py
+++ other/bwf.py
@@ -4,36 +4,47 @@
 
 import math
 
-bwf_file = open("bwf.csv", "r")
 
 def bwf_dict():
     bwf_dictionary = {}
-    total_svcs = 0
-    total_total = 0
-    average = 0
 
     for line in bwf_file:
         split_line = line.split(",")
-        bwf_dictionary["Service Name"] = split_line[0]
-        bwf_dictionary["Product Family"] = split_line[1]
-        bwf_dictionary["MON"] = int(split_line[2].strip("%"))
-        bwf_dictionary["QA"] = int(split_line[3].strip("%"))
-        bwf_dictionary["COST"] = int(split_line[4].strip("%"))
-        bwf_dictionary["COMP"] = int(split_line[5].strip("%"))
-        bwf_dictionary["REL"] = int(split_line[6].strip("%"))
-        bwf_dictionary["CICD"] = int(split_line[7].strip("%"))
-        bwf_dictionary["SEC"] = int(split_line[8].strip("%"))
-        bwf_dictionary["PERF"] = int(split_line[9].strip("%"))
-        bwf_dictionary["ARCH"] = int(split_line[10].strip("%"))
-        bwf_dictionary["TOTAL"] = int(split_line[11].strip("%\n"))
-        print(bwf_dictionary)
+        bwf_dictionary[split_line[0]] = {"Product Family": split_line[1],
+                                         "MON": int(split_line[2].strip("%")),
+                                         "QA": int(split_line[3].strip("%")),
+                                         "COST": int(split_line[4].strip("%")),
+                                         "COMP": int(split_line[5].strip("%")),
+                                         "REL": int(split_line[6].strip("%")),
+                                         "CICD": int(split_line[7].strip("%")),
+                                         "SEC": int(split_line[8].strip("%")),
+                                         "PERF": int(split_line[9].strip("%")),
+                                         "ARCH": int(split_line[10].strip("%")),
+                                         "TOTAL": int(split_line[11].strip("%\n"))}
 
-        total_total =+ bwf_dictionary["TOTAL"]
+    return bwf_dictionary
+
+
+def bwf_average():
+    total_svcs = 0
+    total_total = 0
+
+    for item in bwf_dictionary:
+        total_total += bwf_dictionary[item]["TOTAL"]
         total_svcs += 1
 
     return "Total Services: " + str(total_svcs) + " " "Average BWF Score: " + str(total_total / total_svcs * 100)
 
 
-print(bwf_dict())
+bwf_file = open("bwf.csv", "r")
 
+# create the dictionary
+bwf_dictionary = bwf_dict()
+
+print(bwf_dictionary)
+
+# calculate the average
+print(bwf_average())
+
+# Always close the connections to your files :)
 bwf_file.close()
